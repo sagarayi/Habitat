be27d7235ccf383efeb80fe45a3c24fbbd91312f
sagarayi <ayisagar@gmail.com>
2025-06-02 23:32:03 -0700

    Initial Commit

---
+++ HabitatTests/HabitatTests.swift
@@ -0,0 +1,16 @@
+//
+//  HabitatTests.swift
+//  HabitatTests
+//
+//  Created by Sagar Ayi on 6/2/25.
+//
+
+import Testing
+
+struct HabitatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
