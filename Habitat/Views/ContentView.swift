9d655ada713955e7d8a21826846b2487276d23e8
sagarayi <ayisagar@gmail.com>
2025-06-07 01:09:29 -0700

    In the process of adding an add button

--- Habitat/Views/ContentView.swift
+++ Habitat/Views/ContentView.swift
@@ -15,41 +15,47 @@ struct ContentView: View {
     @State private var currentHabitName: String = ""
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                LazyVStack(spacing: 16) {
-                    ForEach(habits) { habit in
-                        HabitCardView(habit: habit)
-                            .contextMenu {
-                                Button("Delete", role: .destructive) {
-                                    if let index = habits.firstIndex(of: habit) {
-                                        deleteHabit(offsets: IndexSet(integer: index))
+        HStack {
+            VStack(alignment: .leading,  spacing: 20) {
+                Spacer()
+                Text("Habitat")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                ScrollView {
+                    LazyVStack(spacing: 16) {
+                        ForEach(habits) { habit in
+                            HabitCardView(habit: habit)
+                                .contextMenu {
+                                    Button("Delete", role: .destructive) {
+                                        if let index = habits.firstIndex(of: habit) {
+                                            deleteHabit(offsets: IndexSet(integer: index))
+                                        }
                                     }
                                 }
-                            }
+                        }
                     }
                 }
-                .padding()
-            }
-            .listStyle(.plain)
-            .navigationTitle("Habitat")
-            .background(Color.clear)
-            .listRowSeparator(.hidden)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
+                HStack(alignment: .bottom) {
                     Button(action: {showingAlert.toggle()}) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                    .sheet(isPresented: $showingAlert) {
+                        Image(systemName: "plus")
+                    } .sheet(isPresented: $showingAlert) {
                         HabitInputView()
                     }
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .presentationCornerRadius(25.0)
+                    .background(Color.red.opacity(0.7))
+                    .cornerRadius(50)
+                    .shadow(radius: 5)
+                    .fontWidth(.expanded)
                 }
+                .position(CGPoint(x: 330, y: 200))
+
+                
             }
-        } detail: {
-            Text("Select a Habit")
+            .background {
+                LinearGradient(gradient: Gradient(colors: [Color.blue, Color.gray.opacity(0.6)]), startPoint: .bottom, endPoint: .top)
+                    .ignoresSafeArea()
+            }
         }
     }
 
